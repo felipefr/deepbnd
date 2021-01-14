14013d92d3aba5da4d61cf848966a06b4c4d9a90
Felipe Rocha <rocha@mathpc10.epfl.ch>
2021-01-14 23:02:20 +0100

    from cluster to dell

--- deepBoundary/smartGeneration/training_otherValidation.py
+++ deepBoundary/smartGeneration/training_otherValidation.py
@@ -95,7 +95,7 @@ def basicModelTraining(ns, nX, nY, net, fnames, w_l = 1.0, ratio_val = 0.2):
 
 # run_id = 7 # "p4, no validation separated, shuffle, 100 neurons, nY = 40, 2000 epochs"
 # run_id = 8 # "p4, no validation separated, shuffle, 100 neurons, nY = 40, 500 epochs, new loss"
-run_id = 9 # "p4, no validation separated, shuffle, 100 neurons, nY = 40, 500 epochs, new loss"
+run_id = 37 # "p4, no validation separated, shuffle, 100 neurons, nY = 40, 500 epochs, new loss"
 
 f = open("../../../rootDataPath.txt")
 rootData = f.read()[:-1]
@@ -117,9 +117,131 @@ ns = nsval + nsTrain
 ratio_val = nsval/ns
 
 # 48 , p4_volFraction ==> Adam, complete, new loss, 0.2 validation
-net = {'Neurons': 5*[100], 'drps': 7*[0.0], 'activations': ['relu','relu','sigmoid'], 
-        'reg': 0.0, 'lr': 1.0e-5, 'decay' : 1.0, 'epochs': 10} # normally reg = 1e-5
+
+# run_id = 9
+#net = {'Neurons': 5*[100], 'drps': 7*[0.0], 'activations': ['relu','relu','sigmoid'], 
+#        'reg': 0.0, 'lr': 1.0e-5, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+# run_id = 10
+#net = {'Neurons': 5*[20], 'drps': 7*[0.0], 'activations': ['relu','relu','sigmoid'], 
+#        'reg': 0.0, 'lr': 1.0e-5, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
        
+# run_id = 11
+#net = {'Neurons': 4*[20], 'drps': 6*[0.0], 'activations': ['relu','relu','sigmoid'], 
+#        'reg': 0.0, 'lr': 1.0e-5, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+# run_id = 12
+#net = {'Neurons': 3*[20], 'drps': 5*[0.0], 'activations': ['relu','relu','sigmoid'], 
+#        'reg': 0.0, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+# run_id = 13
+#net = {'Neurons': 3*[10], 'drps': 5*[0.0], 'activations': ['relu','relu','sigmoid'], 
+#        'reg': 0.0, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+# run_id = 14
+#net = {'Neurons': 3*[10], 'drps': 5*[0.0], 'activations': ['relu','relu','sigmoid'], 
+#        'reg': 0.0, 'lr': 1.0e-5, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+# run_id = 15
+#net = {'Neurons': 5*[10], 'drps': 7*[0.0], 'activations': ['relu','relu','sigmoid'], 
+#        'reg': 0.0, 'lr': 1.0e-5, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 16
+#net = {'Neurons': 5*[5], 'drps': 7*[0.0], 'activations': ['relu','relu','sigmoid'], 
+#        'reg': 0.0, 'lr': 1.0e-5, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 17
+#net = {'Neurons': 5*[5], 'drps': 7*[0.0], 'activations': ['relu','relu','sigmoid'], 
+#        'reg': 0.0, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+
+#run_id = 18
+net = {'Neurons': 5*[5], 'drps': 7*[0.0], 'activations': ['relu','relu','linear'], 
+        'reg': 0.0, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+
+#run_id = 19
+net = {'Neurons': 5*[10], 'drps': 7*[0.0], 'activations': ['relu','relu','linear'], 
+        'reg': 0.0, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+
+#run_id = 20
+net = {'Neurons': 4*[10], 'drps': 6*[0.0], 'activations': ['relu','relu','linear'], 
+        'reg': 0.0, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 21
+net = {'Neurons': 5*[5], 'drps': 7*[0.0], 'activations': ['leaky_relu','leaky_relu','linear'], 
+        'reg': 0.0, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 22
+net = {'Neurons': 5*[5], 'drps': 7*[0.0], 'activations': ['leaky_relu','leaky_relu','linear'], 
+        'reg': 0.0, 'lr': 1.0e-3, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 23
+net = {'Neurons': 6*[5], 'drps': 8*[0.0], 'activations': ['leaky_relu','leaky_relu','linear'], 
+        'reg': 0.0, 'lr': 1.0e-3, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 24
+net = {'Neurons': 6*[10], 'drps': 8*[0.2], 'activations': ['leaky_relu','relu','linear'], 
+        'reg': 1.0e-4, 'lr': 1.0e-3, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 25
+net = {'Neurons': 6*[20], 'drps': 8*[0.2], 'activations': ['relu','relu','linear'], 
+        'reg': 1.0e-4, 'lr': 1.0e-3, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 26
+net = {'Neurons': 6*[20], 'drps': 8*[0.2], 'activations': ['leaky_relu','relu','linear'], 
+        'reg': 1.0e-4, 'lr': 1.0e-3, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 27
+net = {'Neurons': 6*[20], 'drps': 8*[0.2], 'activations': ['leaky_relu','relu','linear'], 
+        'reg': 0.0, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 28
+net = {'Neurons': 6*[20], 'drps': 8*[0.2], 'activations': ['relu','relu','linear'], 
+        'reg': 1.0e-4, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 29
+net = {'Neurons': 6*[30], 'drps': 8*[0.2], 'activations': ['relu','relu','linear'], 
+        'reg': 1.0e-4, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+
+#run_id = 30
+net = {'Neurons': 5*[50], 'drps': 7*[0.2], 'activations': ['relu','relu','linear'], 
+        'reg': 1.0e-4, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 31
+net = {'Neurons': 5*[50], 'drps': 7*[0.2], 'activations': ['relu','relu','linear'], 
+        'reg': 0.0, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 32
+#net = {'Neurons': 5*[100], 'drps': 7*[0.2], 'activations': ['relu','relu','linear'], 
+#        'reg': 1.0e-4, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 33
+#net = {'Neurons': 5*[50], 'drps': 7*[0.2], 'activations': ['relu','relu','linear'], 
+#        'reg': 1.0e-4, 'lr': 1.0e-3, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+
+#run_id = 34
+net = {'Neurons': 5*[50], 'drps': 7*[0.2], 'activations': ['relu','relu','sigmoid'], 
+        'reg': 1.0e-4, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+#run_id = 35
+net = {'Neurons': 5*[50], 'drps': 7*[0.2], 'activations': ['relu','relu','leaky_relu'], 
+        'reg': 1.0e-4, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+
+#run_id = 36
+net = {'Neurons': 5*[50], 'drps': 7*[0.2], 'activations': ['leaky_relu','relu','leaky_relu'], 
+        'reg': 1.0e-4, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+
+#run_id = 37
+net = {'Neurons': 5*[50], 'drps': 7*[0.2], 'activations': ['leaky_relu','relu','sigmoid'], 
+        'reg': 1.0e-4, 'lr': 1.0e-4, 'decay' : 1.0, 'epochs': 500} # normally reg = 1e-5
+
+
 fnames = {}      
 fnames['suffix_out'] = '_p4_nX{0}_nY{1}_{2}'.format(nX,nY,run_id)
 fnames['prefix_out'] = rootData + '/deepBoundary/smartGeneration/differentModels/'
