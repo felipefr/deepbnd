19d32197ddaa09f31b90587fe47848b871a9eaca
Felipe Rocha <rocha@mathpc10.epfl.ch>
2021-01-31 12:42:03 +0100

    cluster to dell

--- deepBoundary/smartGeneration/Symmetry/generationY_completeSymmetric.py
+++ deepBoundary/smartGeneration/Symmetry/generationY_completeSymmetric.py
@@ -12,8 +12,13 @@ import myHDF5 as myhd
 import meshUtils as meut
 import fenicsUtils as feut
 
-folder = ["/Users", "/home"][1] + "/felipefr/switchdrive/scratch/deepBoundary/smartGeneration/LHS_p4_fullSymmetric/"
-folderBasis = ["/Users", "/home"][1] + "/felipefr/switchdrive/scratch/deepBoundary/smartGeneration/LHS_p4_fullSymmetric/"
+f = open("../../../../rootDataPath.txt")
+rootData = f.read()[:-1]
+f.close()
+
+folder = rootData + "/deepBoundary/smartGeneration/LHS_p4_fullSymmetric/"
+folderBasis = rootData + "/deepBoundary/smartGeneration/LHS_p4_fullSymmetric/"
+
 nameSnaps_simple = folder + 'snapshots_simple.h5'
 nameSnaps = folder + 'snapshots_all.h5'
 nameC = folderBasis + 'C.h5'
@@ -34,38 +39,38 @@ dsRef = Measure('ds', Mref)
 ns = 10240
 Nmax = 160
 
-# Isol, fIsol = myhd.loadhd5_openFile(nameSnaps_simple,'solutions_trans', mode = 'r')
-# os.system('rm ' + nameSnaps)
-# Isol_all, fIsol_all = myhd.zeros_openFile(nameSnaps , (4*ns, Vref.dim()), 'solutions_trans')
+#Isol, fIsol = myhd.loadhd5_openFile(nameSnaps_simple,'solutions_trans', mode = 'r')
+#os.system('rm ' + nameSnaps)
+#Isol_all, fIsol_all = myhd.zeros_openFile(nameSnaps , (4*ns, Vref.dim()), 'solutions_trans')
+#
+#w0 = Function(Vref)
+#g1 = Expression(('-x[0]','x[1]'), degree = 1)
+#g2 = Expression(('x[0]','-x[1]'), degree = 1)
+#g3 = Expression(('-x[0]','-x[1]'), degree = 1)
+#
+#for i in range(ns):
+#    print('symmetrising i = ', i)
+#    w0.vector().set_local(Isol[i,:])
+#    w1 = interpolate(feut.myfog(w0,g1),Vref)
+#    w2 = interpolate(feut.myfog(w0,g2),Vref)
+#    w3 = interpolate(feut.myfog(w0,g3),Vref)
+#    
+#    Isol_all[i,:] = w0.vector().get_local()
+#    Isol_all[ns + i,:] = w1.vector().get_local()
+#    Isol_all[2*ns + i,:] = w2.vector().get_local()
+#    Isol_all[3*ns + i,:] = w3.vector().get_local()
+# 
+#fIsol.close()
+#fIsol_all.close()
 
-# w0 = Function(Vref)
-# g1 = Expression(('-x[0]','x[1]'), degree = 1)
-# g2 = Expression(('x[0]','-x[1]'), degree = 1)
-# g3 = Expression(('-x[0]','-x[1]'), degree = 1)
-
-# for i in range(ns):
-#     print('symmetrising i = ', i)
-#     w0.vector().set_local(Isol[i,:])
-#     w1 = interpolate(feut.myfog(w0,g1),Vref)
-#     w2 = interpolate(feut.myfog(w0,g2),Vref)
-#     w3 = interpolate(feut.myfog(w0,g3),Vref)
-    
-#     Isol_all[i,:] = w0.vector().get_local()
-#     Isol_all[ns + i,:] = w1.vector().get_local()
-#     Isol_all[2*ns + i,:] = w2.vector().get_local()
-#     Isol_all[3*ns + i,:] = w3.vector().get_local()
-    
-# fIsol.close()
-# fIsol_all.close()
-
-# # Computing Correlation Matrix 
-# os.system('rm ' + nameC)
-# C, fC = myhd.zeros_openFile(nameC, (4*ns, 4*ns), 'C')
-# Isol, fIsol = myhd.loadhd5_openFile(nameSnaps,'solutions_trans', mode = 'r')
-# gdb.getCorrelation_fromInterpolationMatricial(C,4*ns, Isol, dotProduct, Vref, dsRef)
-# C = Isol[:,:160]@Isol[:,:160].T
-# fC.close()
-# fIsol.close()
+ # Computing Correlation Matrix 
+#os.system('rm ' + nameC)
+#C, fC = myhd.zeros_openFile(nameC, (4*ns, 4*ns), 'C')
+#Isol, fIsol = myhd.loadhd5_openFile(nameSnaps,'solutions_trans', mode = 'r')
+#gdb.getCorrelation_fromInterpolationMatricial(C,4*ns, Isol, dotProduct, Vref, dsRef)
+#C = Isol[:,:160]@Isol[:,:160].T
+#fC.close()
+#fIsol.close()
 
 # # Computing basis 
 os.system('rm ' + nameWbasis)
