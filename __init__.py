2b46532f08d4de53066f660f4a4cf36371265c93
Felipe <felipemech@gmail.com>
2022-03-13 12:04:40 +0100

    update init

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import os
 
 rootPath = os.path.dirname(os.path.abspath(__file__))
 
-rootDataPath = '/home/felipe/deepBND/DATA'
+rootDataPath = '~/deepBND/DATA'
 #rootDataPath = '/home/felipefr/switchdrive/scratch/deepBND'
 #rootDataPath = '/Users/felipefr/switchdrive/scratch/deepBND'
 
